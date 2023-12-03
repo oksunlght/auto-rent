798c85ca51270d5a709bc513a62346e8d2a28a7b
oksunlght <38954729+oksunlght@users.noreply.github.com>
2023-12-03 10:40:23 +0000

    Deploying to gh-pages from @ oksunlght/car-rental@6e13e20d84c4ae5170e4476c3d8f2aa955b20d75 🚀

--- static/js/229.a5227320.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[229],{229:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Home page"})})}}}]);
-//# sourceMappingURL=229.a5227320.chunk.js.map
--- static/js/736.8bb72eeb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[736],{736:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h1",{children:"Favorite"})})}}}]);
-//# sourceMappingURL=736.8bb72eeb.chunk.js.map
